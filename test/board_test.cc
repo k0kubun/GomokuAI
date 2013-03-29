af7c52e80c6269116d62c5b41c7b02e402ea4150
ikstrm <mail@ikst.jp>
2013-03-29 14:46:41 +0900

    Make tests all green

--- test/board_test.cc
+++ test/board_test.cc
@@ -2,13 +2,13 @@
 #include "../line.cc"
 #include "../board.cc"
 
-// TEST (Board, HasWinner) {
-//   Board board;
-//   for (int i = 0; i < 5; i++) {
-//     board.set_stone(0, i, kStoneBlack);
-//   }
-//   EXPECT_EQ(true, board.HasWinner(kStoneBlack));
-// }
+TEST (Board, HasWinner) {
+  Board board;
+  for (int i = 0; i < 5; i++) {
+    board.set_stone(0, i, kStoneBlack);
+  }
+  EXPECT_EQ(true, board.HasWinner(kStoneBlack));
+}
 
 TEST (Board, StoneNum) {
   Board board;
@@ -20,15 +20,15 @@ TEST (Board, StoneNum) {
   }
 }
 
-// TEST (Board, FindContinuousLineByLength) {
-//   Board board;
-//   Board::Line line;
-//   for (int i = 0; i < 5; i++) {
-//     board.set_stone(0, i, kStoneBlack);
-//     line = board.FindContinuousLineByLength(i + 1, kStoneBlack);
-//     EXPECT_EQ(i + 1, line.ContinuousLength());
-//   }
-// }
+TEST (Board, FindContinuousLineByLength) {
+  Board board;
+  Board::Line line;
+  for (int i = 0; i < 5; i++) {
+    board.set_stone(0, i, kStoneBlack);
+    line = board.FindContinuousLineByLength(i + 1, kStoneBlack);
+    EXPECT_EQ(i + 1, line.ContinuousLength());
+  }
+}
 
 TEST (Board, GetMaxLengthContinuousLine) {
   Board board;
