a20dff3c9cdcc7c2e769e107885adcb0895939ca
ikstrm <mail@ikst.jp>
2013-03-29 12:53:41 +0900

    Change test for Line::ContinuousLength

--- test/line_test.cc
+++ test/line_test.cc
@@ -5,6 +5,12 @@
 TEST (Line, ContinuousLength) {
   Board::Line line(kStoneBlack);
   EXPECT_EQ(0, line.ContinuousLength());
+  for (int i = 0; i < 10; i++) {
+    line.Append(0, i);
+    EXPECT_EQ(i + 1, line.ContinuousLength());
+  }
+  line.Append(0, 11);
+  EXPECT_EQ(10, line.ContinuousLength());
 }
 
 TEST (Line, DiscontinuousLength) {
