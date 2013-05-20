1259c054429af102353cc41b4931231541f82f98
kokubun <takashi_kokubun@tekunodo.jp>
2013-05-20 13:28:04 +0900

    Implement MinMax

--- brain.cc
+++ brain.cc
@@ -6,7 +6,8 @@ Brain::Brain(StoneType own_stone) {
 
 void Brain::PrintBoard(Board board) {
   char c;
-  puts("EDCBA9876543210");
+
+  printf("EDCBA9876543210\n");
   for (int i = 0; i < kBoardSize; i++) {
     for (int j = 0; j < kBoardSize; j++) {
       StoneType stone = board.stone(kBoardSize - j - 1, i);
@@ -23,6 +24,15 @@ void Brain::PrintBoard(Board board) {
   }
 }
 
+Position Brain::MinMaxSearch(Board board) {
+  return GetMinMaxPoint(board, false);
+}
+
+Position Brain::AlphaBetaPruning(Board board) {
+  return GetMinMaxPoint(board, true);
+}
+
+// Original Algorithm
 Position Brain::GetPutPoint(Board board) {
   Board::Line line;
   Position put_point;
@@ -131,17 +141,99 @@ Position Brain::GetPutPoint(Board board) {
   return GetEmptyPoint(board);
 }
 
+void Brain::set_own_stone(StoneType own_stone) {
+  own_stone_ = own_stone;
+}
+
 StoneType Brain::own_stone() {
   return own_stone_;
 }
 
-void Brain::set_own_stone(StoneType own_stone) {
-  own_stone_ = own_stone;
-  opponent_stone_ = OppositeStone(own_stone);
+StoneType Brain::opponent_stone() {
+  return OppositeStone(own_stone_);
 }
 
-StoneType Brain::opponent_stone() {
-  return opponent_stone_;
+bool Brain::IsMyTurn(int count) {
+  return count % 2 == 0;
+}
+
+StoneType Brain::TurnStone(int count) {
+  if (IsMyTurn(count) == true) {
+    return own_stone();
+  } else {
+    return opponent_stone();
+  }
+}
+
+int Brain::Heuristic(Board board, int count) {
+  int num = 0;
+  for (int i = 4; i < 10; i++) {
+    for (int j = 4; j < 10; j++) {
+      if (board.stone(i, j) == own_stone()) {
+        num++;
+      }
+    }
+  }
+  return num;
+}
+
+// If count is even, return Brain's max heuristic
+// If count is odd,  return enemy's min heuristic
+int Brain::MinMax(Board board, int count, bool alpha_beta) {
+  Board virtual_board;
+  int best_min_max, current_min_max;
+
+  if (IsMyTurn(count) == true) {
+    best_min_max = 0;
+  } else {
+    best_min_max = 100;
+  }
+
+  for (int i = 0; i < kBoardSize; i++) {
+    for (int j = 0; j < kBoardSize; j++) {
+      if (board.stone(i, j) != kStoneBlank || board.IsBannedPoint(i, j, TurnStone(count))) {
+        continue;
+      }
+
+      virtual_board = board;
+      virtual_board.set_stone(i, j, TurnStone(count));
+      if (count == 0) {
+        current_min_max = Heuristic(virtual_board, count);
+      } else {
+        current_min_max = MinMax(virtual_board, count - 1, alpha_beta);
+      }
+
+      if (IsMyTurn(count) == true) {
+        if (current_min_max > best_min_max) {
+          best_min_max = current_min_max;
+        }
+      } else {
+        if (current_min_max < best_min_max) {
+          best_min_max = current_min_max;
+        }
+      }
+    }
+  }
+  return best_min_max;
+}
+
+Position Brain::GetMinMaxPoint(Board board, bool alpha_beta) {
+  Position put_point;
+  Board virtual_board;
+  int max_min_max = 0, current_min_max;
+
+  for (int i = 0; i < kBoardSize; i++) {
+    for (int j = 0; j < kBoardSize; j++) {
+      virtual_board = board;
+      virtual_board.set_stone(i, j, own_stone());
+      current_min_max = MinMax(virtual_board, 0, alpha_beta);
+      if (current_min_max > max_min_max) {
+        max_min_max = current_min_max;
+        put_point = Position(i, j);
+      }
+    }
+  }
+  return put_point;
 }
 
 Position Brain::GetEmptyPoint(Board board) {
--- brain.h
+++ brain.h
@@ -7,19 +7,25 @@ class Brain {
  public:
   Brain(StoneType own_stone);
 
-  void PrintBoard(Board board);
+  void     PrintBoard(Board board);
+  Position MinMaxSearch(Board board);
+  Position AlphaBetaPruning(Board board);
   Position GetPutPoint(Board board);
 
+  void      set_own_stone(StoneType own_stone);
   StoneType own_stone();
-  void set_own_stone(StoneType own_stone);
   StoneType opponent_stone();
 
  private:
-  Position GetEmptyPoint(Board board);
+  bool      IsMyTurn(int count);
+  StoneType TurnStone(int count);
+  int       Heuristic(Board board, int count);
+  int       MinMax(Board board, int count, bool alpha_beta);
+  Position  GetMinMaxPoint(Board board, bool alpha_beta);
+  Position  GetEmptyPoint(Board board);
   StoneType OppositeStone(StoneType stone);
 
   StoneType own_stone_;
-  StoneType opponent_stone_;
 };
 
 #endif
